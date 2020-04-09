db05389e3ee631f92a9f3d84becb13c11d033ad3
uliamalkova256 <uliamalkova256@gmail.com>
2020-04-09 18:08:02 +0400

    fix

--- Conteynerovoz/Conteynerovoz.cs
+++ Conteynerovoz/Conteynerovoz.cs
@@ -6,13 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsFormCars
-{ public enum Direction
-        {
-            Up,
-            Down,
-            Left,
-            Right
-        }
+{
+
     class Conteynerovoz
     {
         private float _startPosX;
@@ -27,7 +22,7 @@ namespace WindowsFormCars
         public Color DopColor { private set; get; }
         public bool Cran { private set; get; }
         public bool Anchor { private set; get; }
-public bool Flag { private set; get; }
+        public bool Flag { private set; get; }
         public Conteynerovoz(int maxSpeed, float weight, Color mainColor, Color dopColor, bool cran, bool anchor, bool flag)
         {
             MaxSpeed = maxSpeed;
@@ -35,13 +30,13 @@ public bool Flag { private set; get; }
             MainColor = mainColor;
             DopColor = dopColor;
             Cran = cran;
-           Anchor = anchor;
+            Anchor = anchor;
             Flag = flag;
         }
         public void SetPosition(int x, int y, int width, int height)
         {
-            _startPosX = x+20;
-            _startPosY = y+20;
+            _startPosX = x + 20;
+            _startPosY = y + 20;
             _pictureWidth = width;
             _pictureHeight = height;
         }
@@ -58,8 +53,8 @@ public bool Flag { private set; get; }
                     break;
                 case Direction.Left:
                     if (_startPosX - step > 20)
-                        
-                {
+
+                    {
                         _startPosX -= step;
                     }
                     break;
@@ -80,50 +75,50 @@ public bool Flag { private set; get; }
         public void DrawCar(Graphics g)
         {
             Pen pen = new Pen(MainColor);
-            Brush br = new SolidBrush(MainColor);                     g.FillRectangle(br, _startPosX, _startPosY + 70, 150, 30);
-           g.FillRectangle(br, _startPosX+150, _startPosY + 50, 70, 50);
-             for (float m= _startPosX + 220;m< _startPosX + 240;m++)
-             {
-               g.DrawLine(pen, m, _startPosY + 50, _startPosX + 220, _startPosY + 100);
-             }
-            for (float m = _startPosX-20 ; m <= _startPosX; m++)
+            Brush br = new SolidBrush(MainColor); g.FillRectangle(br, _startPosX, _startPosY + 70, 150, 30);
+            g.FillRectangle(br, _startPosX + 150, _startPosY + 50, 70, 50);
+            for (float m = _startPosX + 220; m < _startPosX + 240; m++)
             {
-           g.DrawLine(pen, m, _startPosY + 70, _startPosX, _startPosY + 100);
-          }
+                g.DrawLine(pen, m, _startPosY + 50, _startPosX + 220, _startPosY + 100);
+            }
+            for (float m = _startPosX - 20; m <= _startPosX; m++)
+            {
+                g.DrawLine(pen, m, _startPosY + 70, _startPosX, _startPosY + 100);
+            }
             Brush brRed = new SolidBrush(Color.Red);
-            g.FillRectangle(brRed, _startPosX+130, _startPosY + 50, 20, 20);       
-            g.FillRectangle(brRed, _startPosX+90, _startPosY + 50, 20, 20);
-            g.FillRectangle(brRed, _startPosX+110, _startPosY + 30, 20, 20);
+            g.FillRectangle(brRed, _startPosX + 130, _startPosY + 50, 20, 20);
+            g.FillRectangle(brRed, _startPosX + 90, _startPosY + 50, 20, 20);
+            g.FillRectangle(brRed, _startPosX + 110, _startPosY + 30, 20, 20);
             Brush brGr = new SolidBrush(Color.Green);
-            g.FillRectangle(brGr, _startPosX+90, _startPosY + 30, 20, 20);           
-            g.FillRectangle(brGr, _startPosX+110, _startPosY + 50, 20, 20);
+            g.FillRectangle(brGr, _startPosX + 90, _startPosY + 30, 20, 20);
+            g.FillRectangle(brGr, _startPosX + 110, _startPosY + 50, 20, 20);
             Brush brWh = new SolidBrush(Color.White);
             g.FillRectangle(brWh, _startPosX, _startPosY + 10, 50, 60);
             g.FillRectangle(br, _startPosX, _startPosY, 70, 10);
-            g.DrawLine(pen, _startPosX, _startPosY +10, _startPosX, _startPosY + 70);
-            g.DrawLine(pen, _startPosX+50, _startPosY + 10, _startPosX+50, _startPosY + 70);
+            g.DrawLine(pen, _startPosX, _startPosY + 10, _startPosX, _startPosY + 70);
+            g.DrawLine(pen, _startPosX + 50, _startPosY + 10, _startPosX + 50, _startPosY + 70);
             if (Cran)
             {
                 Brush spoiler = new SolidBrush(DopColor);
-                g.FillRectangle(spoiler, _startPosX+50 , _startPosY-20, 10, 20);
+                g.FillRectangle(spoiler, _startPosX + 50, _startPosY - 20, 10, 20);
                 g.FillRectangle(spoiler, _startPosX + 50, _startPosY - 30, 35, 10);
                 g.DrawRectangle(pen, _startPosX + 50, _startPosY - 20, 10, 20);
                 g.DrawRectangle(pen, _startPosX + 50, _startPosY - 30, 35, 10);
-                g.DrawLine(pen, _startPosX + 80, _startPosY -20, _startPosX + 80, _startPosY + 30);
-                g.DrawArc(pen, _startPosX + 70, _startPosY + 25, 10, 10, 0,140);
+                g.DrawLine(pen, _startPosX + 80, _startPosY - 20, _startPosX + 80, _startPosY + 30);
+                g.DrawArc(pen, _startPosX + 70, _startPosY + 25, 10, 10, 0, 140);
             }
             if (Anchor)
             {
-               
+
                 Pen penWh = new Pen(Color.Red);
                 g.DrawLine(penWh, _startPosX, _startPosY + 80, _startPosX, _startPosY + 110);
-                g.DrawLine(penWh, _startPosX-5, _startPosY +95, _startPosX +5, _startPosY + 95);
-                g.DrawArc(penWh, _startPosX-10, _startPosY + 100, 20, 10, 0, 180);
+                g.DrawLine(penWh, _startPosX - 5, _startPosY + 95, _startPosX + 5, _startPosY + 95);
+                g.DrawArc(penWh, _startPosX - 10, _startPosY + 100, 20, 10, 0, 180);
             }
             if (Flag)
             {
                 Pen penWh = new Pen(Color.Red);
-                g.DrawLine(penWh, _startPosX+220, _startPosY + 20, _startPosX+220, _startPosY + 50);
+                g.DrawLine(penWh, _startPosX + 220, _startPosY + 20, _startPosX + 220, _startPosY + 50);
                 for (float m = _startPosY + 20; m < _startPosY + 30; m++)
                 {
                     g.DrawLine(pen, _startPosX + 210, _startPosY + 25, _startPosX + 220, m);
